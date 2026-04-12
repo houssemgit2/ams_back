67081d2cdac3669c1f2c4d11f877b7cb70e6e137
houssemgit2 <fethiaboulfarah@gmail.com>
2026-04-12 12:38:51 +0200

    fix:create provider code

--- src/main/java/com/sip/ams/controllers/ProviderController.java
+++ src/main/java/com/sip/ams/controllers/ProviderController.java
@@ -6,6 +6,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,28 +21,28 @@ import com.sip.ams.repositories.ProviderRepository;
 @RequestMapping("providers")
 public class ProviderController {
 
-	@Autowired
-	ProviderRepository providerRepository;
+    @Autowired
+    ProviderRepository providerRepository;
 
-	@GetMapping("/")
-	@Operation(summary = "Récupération de tous les providers")
-	@ApiResponses(value = {
-			@ApiResponse(responseCode = "200",description = "Succès de get All"),
-			@ApiResponse(responseCode = "404",description = "Provider non trouvé"),
+    @GetMapping("/")
+    @Operation(summary = "Récupération de tous les providers")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Succès de get All"),
+            @ApiResponse(responseCode = "404", description = "Provider non trouvé"),
 
-	})
-	public List<Provider> getAllProviders() {
-		return (List<Provider>) providerRepository.findAll();
-	}
+    })
+    public List<Provider> getAllProviders() {
+        return (List<Provider>) providerRepository.findAll();
+    }
 
-	@PostMapping("/")
-	@Operation(summary = "Créer un provider")
-	@ApiResponses(value = {
-			@ApiResponse(responseCode = "200",description = "Succès de addProvider"),
+    @PostMapping("/")
+    @Operation(summary = "Créer un provider")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Succès de addProvider"),
 
-	})
-	public Provider addProvider(@RequestBody Provider p) {
-		return providerRepository.save(p);
-	}
+    })
+    public ResponseEntity<Provider> addProvider(@RequestBody Provider p) {
+        return new ResponseEntity<Provider>(providerRepository.save(p), HttpStatus.CREATED);
+    }
 
 }
